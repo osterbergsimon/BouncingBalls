c29ea34493581544e40391b1ccae20eb65966979
Simon Österberg <blu3dx@gmail.com>
2014-04-02 14:53:59 +0200

    Implemented gravity, started working on collision

--- src/Ball.java
+++ src/Ball.java
@@ -2,7 +2,7 @@
 public class Ball {
 	
 	private final double weight, r;
-	private double x, y, vx, vy;
+	private double x, y, vx, vy, ax, ay;
 
 	
 	public Ball(double radius, double weight, double x, double y){
@@ -12,6 +12,8 @@ public class Ball {
 		this.vx = 2.3;
 		this.vy = 1;
 		this.r = radius;
+		this.ax = 1;
+		this.ay = -9.82;
 		
 	}
 
@@ -53,6 +55,23 @@ public class Ball {
 
 	public void setVy(double vy) {
 		this.vy = vy;
+	}
+	
+	public double getAy() {
+		return ay;
+	}
+
+
+	public void setAy(double ay) {
+		this.ay = ay;
+	}
+	public double getAx() {
+		return ax;
+	}
+
+
+	public void seAx(double ax) {
+		this.ax = ax;
 	}
 
 
--- src/BallModel.java
+++ src/BallModel.java
@@ -6,34 +6,73 @@ public class BallModel implements IBouncingBallsModel {
 
 	private final double areaWidth;
 	private final double areaHeight;
-	private Ball ball1,ball2;
+	private Ball b1,b2;
 	private Ball[] balls;
 	private final double g;
 
 	public BallModel(double width, double height) {
 		this.areaWidth = width;
 		this.areaHeight = height;
-		balls = new Ball[2];
-		balls[0] = new Ball(1,3,1,5);
-		balls[1] = new Ball(3,5,10,10);
+		b1 = new Ball(1,3,1,5);
+		b2 = new Ball(3,5,10,10);
+		balls = new Ball[1];
+		balls[0] = b1;
+		balls[1] = b2;
 		this.g = -9.82;
 	}
 
 	@Override
 	public void tick(double deltaT) {
 		for(Ball b : balls){
+			
+			
+			System.out.println("1:"+b.getVy());
 			if (b.getX() < b.getR() || b.getX() > areaWidth - b.getR()) {
 				b.setVx(-1*b.getVx());
 			}
 			if (b.getY() < b.getR() || b.getY() > areaHeight - b.getR()) {
 				b.setVy(-1*b.getVy());
+				System.out.println("BLÖH!");
+			}else{
+				b.setVy(calculateSpeed(b.getVy(),b.getAy(),deltaT));
 			}
 			
+			/*if(b.getY() <= b.getR()){
+				if(b.getVy() <= 0){
+					b.setVy(-1*b.getVy());
+					System.out.println("2:"+b.getVy());
+				}
+			}else{
+				b.setVy(calculateSpeed(b.getVy(),b.getAy(),deltaT));
+				
+			}
+			
+			if(b.getY() > areaHeight - b.getR()){
+				if(b.getVy() > 0){
+					b.setVy(-1*b.getVy());
+				}
+			}else{
+				b.setVy(calculateSpeed(b.getVy(),b.getAy(),deltaT));
+			}*/
+			
+			b.setY(b.getY()+b.getVy()*deltaT);
 			b.setX(b.getX()+b.getVx()*deltaT);
-			b.setY(b.getY()+(b.getVy()+g*deltaT)*deltaT);
+			
+			
+			//System.out.println(b.getVy());
+		}
+		
+		if(Math.sqrt(
+				Math.pow((b1.getX()-b2.getX()),2)+Math.pow((b1.getY()-b2.getY()),2)) 
+				<= b1.getR()+b2.getR()){
+			
 		}
 	
 
+	}
+	
+	private double calculateSpeed(double speed, double acc, double deltaT){
+		return speed+(acc*deltaT);
 	}
 
 	@Override
