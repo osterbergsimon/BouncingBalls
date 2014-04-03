61918543c76009f20a98322e21940f2d494557ab
Simon Österberg <blu3dx@gmail.com>
2014-04-03 17:10:15 +0200

    Fixed collisions for two balls

--- src/BallModel.java
+++ src/BallModel.java
@@ -9,12 +9,13 @@ public class BallModel implements IBouncingBallsModel {
 	private Ball b1,b2;
 	private Ball[] balls;
 	private final double g;
+	boolean thisTick = true;
 
 	public BallModel(double width, double height) {
 		this.areaWidth = width;
 		this.areaHeight = height;
-		b1 = new Ball(1,3,1,5);
-		b2 = new Ball(3,5,10,10);
+		b1 = new Ball(1,1,1,3);
+		b2 = new Ball(2,8,10,10);
 		balls = new Ball[2];
 		balls[0] = b1;
 		balls[1] = b2;
@@ -23,17 +24,31 @@ public class BallModel implements IBouncingBallsModel {
 
 	@Override
 	public void tick(double deltaT) {
+				
+		if(isCollision(b1,b2)){
+			System.out.println(b1.getWeight()*b1.getVy()+
+					b2.getWeight()*b2.getVy()+
+					b1.getWeight()*b1.getVx()+
+					b2.getWeight()*b2.getVx());
+			resolveCollision(b1,b2);
+			System.out.println(b1.getWeight()*b1.getVy()+
+					b2.getWeight()*b2.getVy()+
+					b1.getWeight()*b1.getVx()+
+					b2.getWeight()*b2.getVx());
+			thisTick = false;
+		}
+		
 		for(Ball b : balls){
 			
-			
-			System.out.println("1:"+b.getVy());
 			if (b.getX() < b.getR() || b.getX() > areaWidth - b.getR()) {
 				b.setVx(-1*b.getVx());
+				thisTick = false;
 			}
 			if (b.getY() < b.getR() || b.getY() > areaHeight - b.getR()) {
 				b.setVy(-1*b.getVy());
-				System.out.println("BLÖH!");
-			}else{
+				thisTick = false;
+			}	
+			if(thisTick){
 				b.setVy(calculateSpeed(b.getVy(),b.getAy(),deltaT));
 			}
 			
@@ -42,16 +57,19 @@ public class BallModel implements IBouncingBallsModel {
 			b.setX(b.getX()+b.getVx()*deltaT);
 			
 			
+			
 			//System.out.println(b.getVy());
 		}
 		
-		if(Math.sqrt(
-				Math.pow((b1.getX()-b2.getX()),2)+Math.pow((b1.getY()-b2.getY()),2)) 
-				<= b1.getR()+b2.getR()){
-			resolveCollision(b1,b2);
-		}
+		thisTick = true;
 	
 
+	}
+	
+	private boolean isCollision(Ball b1, Ball b2){
+		return (Math.sqrt(
+				Math.pow((b1.getX()-b2.getX()),2)+Math.pow((b1.getY()-b2.getY()),2)) 
+					<= b1.getR()+b2.getR());
 	}
 	
 	private void resolveCollision(Ball b1, Ball b2){
@@ -71,11 +89,19 @@ public class BallModel implements IBouncingBallsModel {
 	//  Projection of the velocities in these axes.
 	    double   va1 = vx1 * ax + vy1 * ay, vb1 = -vx1 * ay + vy1 * ax;
 	    double   va2 = vx2 * ax + vy2 * ay, vb2 = -vx2 * ay + vy2 * ax;
+	    double m = b1.getWeight()*va1 + b2.getWeight()*va2;
+	    double r = -(va2-va1);
+	    
+	//  New velocities in these axes (after collision);
+	    //double   vaP1 = va1 + (va2 - va1) / (b1.getWeight() / b2.getWeight());
+	    //double   vaP2 = va2 + (va1 - va2) / (b2.getWeight() / b1.getWeight());
+	    //double vaP1 = (m-b2.getWeight()*((r*b1.getWeight()+m)/(b1.getWeight()+b2.getWeight()))/b1.getWeight());
+	    //double vaP2 = (r*b1.getWeight()+m)/(b1.getWeight()+b2.getWeight());
 
-	//  New velocities in these axes (after collision): edmass / b2->mass);
-	    double   vaP1 = va1 + (va2 - va1) / (b1.getWeight() / b2.getWeight());
-	    double   vaP2 = va2 + (va1 - va2) / (b2.getWeight() / b1.getWeight());
-
+	    double vaP1 = (m-b2.getWeight()*r)/(b1.getWeight()+b2.getWeight());
+	    double vaP2 = (m-b2.getWeight()*r)/(b1.getWeight()+b2.getWeight()) +r;
+	    
+	    
 	//  Undo the projections.
 	    vx1 = vaP1 * ax - vb1 * ay; 
 	    vy1 = vaP1 * ay + vb1 * ax; // new vx,vy for ball 1 after collision.
